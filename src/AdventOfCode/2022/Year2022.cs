86175e7f4c8c7ceab03e3189c0ee8a379327585d
Aaron <aaron@amorgan.net.nz>
2022-12-02 19:22:43 +1300

    Day 1, part 2.

--- src/AdventOfCode/2022/Year2022.cs
+++ src/AdventOfCode/2022/Year2022.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode
         [Theory]
         [InlineData("Day1DevelopmentTesting.txt", 24000)]
         [InlineData("Day1.txt", 71924)]
-        public void Day1_CalorieCounting(string filename, int expectedAnswer)
+        public void Day1_Part1_CalorieCounting(string filename, int expectedAnswer)
         {
             int maxCaloriesCarried = 0;
             int currentCalories = 0;
@@ -26,7 +26,33 @@ namespace AdventOfCode
 
             Assert.Equal(expectedAnswer, maxCaloriesCarried);
         }
+
+        [Theory]
+        [InlineData("Day1DevelopmentTesting.txt", 45000)]
+        [InlineData("Day1.txt", 210406)]
+        public void Day1_Part2_CalorieCounting(string filename, int expectedAnswer)
+        {
+            List<int> elfCalories = new();
+            int currentCalories = 0;
+
+            foreach (var line in File.ReadLines($"./TestData/{filename}"))
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+
+                    elfCalories.Add(currentCalories);
+                    currentCalories = 0;
+                    continue;
+                }
+
+                currentCalories += int.Parse(line);
+
+            }
+
+            elfCalories.Add(currentCalories);
+            elfCalories.Sort();
+
+            Assert.Equal(expectedAnswer, elfCalories.TakeLast(3).Sum());
+        }
     }
-
-
 }
