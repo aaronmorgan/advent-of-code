fdb52b33b45a412b48551548ab374e9ed5c81d64
Aaron <aaron@amorgan.net.nz>
2023-12-15 22:37:44 +1300

    2023, day 14, part 1.

--- src/AdventOfCode/Utilities/FileLoader.cs
+++ src/AdventOfCode/Utilities/FileLoader.cs
@@ -2,5 +2,10 @@
 
 public static class FileLoader
 {
+    [Obsolete("Use ReadAllLines instead")]
     public static IEnumerable<string> ReadFile(string filename) => File.ReadAllLines($"./TestData/{filename}");
+
+    public static IEnumerable<string> ReadAllLines(string filename) => File.ReadAllLines($"./TestData/{filename}");
+
+    public static string ReadAllText(string filename) => File.ReadAllText($"./TestData/{filename}");
 }
