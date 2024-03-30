3acaf456470282c4a0805880645b47e0f3b2aba9
Aaron <aaron@amorgan.net.nz>
2024-03-31 07:26:20 +1300

    2023, day 18, part 2.

--- src/AdventOfCode/2023/Day18.cs
+++ src/AdventOfCode/2023/Day18.cs
@@ -1,5 +1,4 @@
-﻿using System.Text;
-using AdventOfCode.Algorithms;
+﻿using AdventOfCode.Algorithms;
 using AdventOfCode.Utilities;
 
 namespace AdventOfCode._2023;
@@ -26,26 +25,10 @@ public class Day18
 
             switch (direction)
             {
-                case "R":
-                {
-                    x += distance;
-                    break;
-                }
-                case "L":
-                {
-                    x -= distance;
-                    break;
-                }
-                case "D":
-                {
-                    y += distance;
-                    break;
-                }
-                case "U":
-                {
-                    y -= distance;
-                    break;
-                }
+                case "R": { x += distance; break; }
+                case "L": { x -= distance; break; }
+                case "D": { y += distance; break; }
+                case "U": { y -= distance; break; }
             }
 
             points[rowIndex + 1] = new ShoelaceFormula.Point(x, y);
@@ -58,172 +41,41 @@ public class Day18
         Assert.Equal(polygonArea, expectedAnswer);
     }
 
-    [Theory(Skip = "")]
+    [Theory]
     [InlineData("Day18DevelopmentTesting1.txt", 952408144115)]
-    //   [InlineData("Day18.txt", 34329)]
+    [InlineData("Day18.txt", 42617947302920)]
     public void Day18_Part2_LavaductLagoon(string filename, long expectedAnswer)
     {
         var input = FileLoader.ReadAllLines("2023/" + filename).ToList();
 
-        int x = 0, y = 0, maxX = 0, maxY = 0, startX = 0, startY = 0;
+        int x = 0, y = 0, boundaryLength = 0;
 
-        // Determine the maximum Right and Down steps so we know the size of the ground map.
-        foreach (var row in input)
+        var points = new ShoelaceFormula.Point[input.Count + 1];
+        points[0] = new ShoelaceFormula.Point(0, 0);
+
+        for (var rowIndex = 0; rowIndex < input.Count; rowIndex++)
         {
-            var instructions = row.Split('#')[1].Trim('#', ')');
+            var instructions = input[rowIndex].Split('#')[1].Trim('#', ')');
             var direction = instructions[^1];
             var distance = Convert.ToInt32(instructions[..5], 16);
 
-            switch (direction)
-            {
-                case '0': // Right
-                {
-                    x += distance;
-                    break;
-                }
-                case '1': // Down
-                {
-                    y += distance;
-                    break;
-                }
-                case '2': // Left
-                {
-                    x -= distance;
-                    break;
-                }
-
-                case '3': // Up
-                {
-                    y -= distance;
-                    break;
-                }
-            }
-
-            if (x < 0) startX = Math.Max(startX, Math.Abs(x));
-            if (y < 0) startY = Math.Max(startY, Math.Abs(y));
-
-            maxX = Math.Max(maxX, x);
-            maxY = Math.Max(maxY, y);
-        }
-
-        // Pre fill the ground map with dirt.
-        int yLineCount = startY + maxY + 1;
-        string xLine = new string('.', startX + maxX + 1);
-        List<StringBuilder> groundMap = new(yLineCount);
-
-        for (int i = 0; i < yLineCount; i++)
-        {
-            groundMap.Add(new StringBuilder(xLine));
-        }
-
-        // Dig the trench...
-        x = startX;
-        y = startY;
-
-        foreach (var row in input)
-        {
-            var instructions = row.Split('#')[1].Trim('#', ')');
-            var direction = instructions[^1];
-            var distance = Convert.ToInt32(instructions[..5], 16);
+            boundaryLength += distance;
 
             switch (direction)
             {
-                case '0': // Right
-                {
-                    for (var i = 0; i < distance; i++)
-                    {
-                        x++;
-                        groundMap[y][4] = '#';
-                    }
-
-                    break;
-                }
-                case '1': // Down
-                {
-                    for (var i = 0; i < distance; i++)
-                    {
-                        y++;
-                        groundMap[y][x] = '#';
-                    }
-
-                    break;
-                }
-                case '2': // Left
-                {
-                    for (var i = 0; i < distance; i++)
-                    {
-                        x--;
-                        groundMap[y][x] = '#';
-                    }
-
-                    break;
-                }
-                case '3': // Up
-                {
-                    for (var i = 0; i < distance; i++)
-                    {
-                        y--;
-                        groundMap[y][x] = '#';
-                    }
-
-                    break;
-                }
+                case '0': { x += distance; break; }
+                case '1': { y += distance; break; }
+                case '2': { x -= distance; break; }
+                case '3': { y -= distance; break; }
             }
+
+            points[rowIndex + 1] = new ShoelaceFormula.Point(x, y);
         }
 
-        // Dig out interior...
-        for (var i = 0; i < groundMap.Count; i++)
-        {
-            var insideTrench = false;
-            char lastTile = '.';
+        points[^1] = new ShoelaceFormula.Point(points[0].X, points[0].Y);
 
-            for (var j = 0; j < groundMap[i].Length; j++)
-            {
-                var currentTile = groundMap[i][j];
-                var nextTile = j == groundMap[i].Length - 1 ? '#' : groundMap[i][j + 1];
+        var polygonArea = ShoelaceFormula.CalculatePolygonArea(points, boundaryLength);
 
-                switch (currentTile)
-                {
-                    // Encounter the trench edge afer not digging, start digging.
-                    case '#' when lastTile == '.' && nextTile == '.' && !insideTrench:
-                        insideTrench = true;
-                        break;
-
-                    case '#' when lastTile == '.' && nextTile == '.' && insideTrench:
-                        insideTrench = false;
-                        break;
-                    case '#' when lastTile == '#' && nextTile == '.':
-                        // Look above us to see if we at at the ┘ corner of the trench boundry and therefore the next tile 
-                        // is actually within the trench boundy.
-                        if (i > 0 && groundMap[i - 1][j + 1] == 'O')
-                        {
-                            insideTrench = true;
-                        }
-                        else insideTrench = false;
-
-                        break;
-
-                    case '.' when insideTrench:
-                        // Dig...
-                        groundMap[i][j] = 'O';
-                        break;
-                }
-
-                lastTile = currentTile;
-            }
-        }
-
-        // Count the size of the interior...
-        int result = 0;
-
-        for (var i = 0; i < groundMap.Count; i++)
-        {
-            for (var j = 0; j < groundMap[i].Length; j++)
-            {
-                if (groundMap[i][j] == '#' || groundMap[i][j] == 'O') result++;
-            }
-        }
-
-        Assert.Equal(expectedAnswer, result);
+        Assert.Equal(polygonArea, expectedAnswer);
     }
 }
