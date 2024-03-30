1e8a2446fe6d28828115a28646b69d1d3cc83421
Aaron <aaron@amorgan.net.nz>
2024-03-31 00:16:35 +1300

    Update 2023, day 18, part 1 to use Shoelace forumla.

--- src/AdventOfCode/2023/Day18.cs
+++ src/AdventOfCode/2023/Day18.cs
@@ -1,4 +1,5 @@
-﻿using AdventOfCode.Extensions;
+﻿using System.Text;
+using AdventOfCode.Algorithms;
 using AdventOfCode.Utilities;
 
 namespace AdventOfCode._2023;
@@ -7,19 +8,21 @@ public class Day18
 {
     [Theory]
     [InlineData("Day18DevelopmentTesting1.txt", 62)]
-    [InlineData("Day18.txt", 34329)] // 31057 31058 too low. 36552 too high   34330
-    public void Day19_Part1_LavaductLagoon(string filename, int expectedAnswer)
+    [InlineData("Day18.txt", 34329)]
+    public void Day18_Part1_LavaductLagoon(string filename, int expectedAnswer)
     {
         var input = FileLoader.ReadAllLines("2023/" + filename).ToList();
 
-        int x = 0, y = 0, maxX = 0, maxY = 0, startX = 0, startY = 0;
+        int x = 0, y = 0, boundaryLength = 0;
 
-        // Determine the maximum Right and Down steps so we know the size of the ground map.
-        foreach (var row in input)
+        var points = new ShoelaceFormula.Point[input.Count + 1];
+        points[0] = new ShoelaceFormula.Point(0, 0);
+
+        for (var rowIndex = 0; rowIndex < input.Count; rowIndex++)
         {
-            var instructions = row.Split(' ');
-            var direction = instructions[0];
-            var distance = int.Parse(instructions[1]);
+            var (direction, distance) = input[rowIndex].Split(' ') switch { var str => (str[0], int.Parse(str[1])) };
+
+            boundaryLength += distance;
 
             switch (direction)
             {
@@ -45,6 +48,57 @@ public class Day18
                 }
             }
 
+            points[rowIndex + 1] = new ShoelaceFormula.Point(x, y);
+        }
+
+        points[^1] = new ShoelaceFormula.Point(points[0].X, points[0].Y);
+
+        var polygonArea = ShoelaceFormula.CalculatePolygonArea(points, boundaryLength);
+
+        Assert.Equal(polygonArea, expectedAnswer);
+    }
+
+    [Theory(Skip = "")]
+    [InlineData("Day18DevelopmentTesting1.txt", 952408144115)]
+    //   [InlineData("Day18.txt", 34329)]
+    public void Day18_Part2_LavaductLagoon(string filename, long expectedAnswer)
+    {
+        var input = FileLoader.ReadAllLines("2023/" + filename).ToList();
+
+        int x = 0, y = 0, maxX = 0, maxY = 0, startX = 0, startY = 0;
+
+        // Determine the maximum Right and Down steps so we know the size of the ground map.
+        foreach (var row in input)
+        {
+            var instructions = row.Split('#')[1].Trim('#', ')');
+            var direction = instructions[^1];
+            var distance = Convert.ToInt32(instructions[..5], 16);
+
+            switch (direction)
+            {
+                case '0': // Right
+                {
+                    x += distance;
+                    break;
+                }
+                case '1': // Down
+                {
+                    y += distance;
+                    break;
+                }
+                case '2': // Left
+                {
+                    x -= distance;
+                    break;
+                }
+
+                case '3': // Up
+                {
+                    y -= distance;
+                    break;
+                }
+            }
+
             if (x < 0) startX = Math.Max(startX, Math.Abs(x));
             if (y < 0) startY = Math.Max(startY, Math.Abs(y));
 
@@ -52,15 +106,14 @@ public class Day18
             maxY = Math.Max(maxY, y);
         }
 
-        var groundMap = new char[startY + maxY + 1, startX + maxX + 1];
-
         // Pre fill the ground map with dirt.
-        for (var i = 0; i < groundMap.GetLength(0); i++)
+        int yLineCount = startY + maxY + 1;
+        string xLine = new string('.', startX + maxX + 1);
+        List<StringBuilder> groundMap = new(yLineCount);
+
+        for (int i = 0; i < yLineCount; i++)
         {
-            for (var j = 0; j < groundMap.GetLength(1); j++)
-            {
-                groundMap[i, j] = '.';
-            }
+            groundMap.Add(new StringBuilder(xLine));
         }
 
         // Dig the trench...
@@ -69,49 +122,48 @@ public class Day18
 
         foreach (var row in input)
         {
-            var instructions = row.Split(' ');
-            var direction = instructions[0];
-            var distance = int.Parse(instructions[1]);
-
+            var instructions = row.Split('#')[1].Trim('#', ')');
+            var direction = instructions[^1];
+            var distance = Convert.ToInt32(instructions[..5], 16);
 
             switch (direction)
             {
-                case "R":
+                case '0': // Right
                 {
                     for (var i = 0; i < distance; i++)
                     {
                         x++;
-                        groundMap[y, x] = '#';
+                        groundMap[y][4] = '#';
                     }
 
                     break;
                 }
-                case "L":
-                {
-                    for (var i = 0; i < distance; i++)
-                    {
-                        x--;
-                        groundMap[y, x] = '#';
-                    }
-
-                    break;
-                }
-                case "D":
+                case '1': // Down
                 {
                     for (var i = 0; i < distance; i++)
                     {
                         y++;
-                        groundMap[y, x] = '#';
+                        groundMap[y][x] = '#';
                     }
 
                     break;
                 }
-                case "U":
+                case '2': // Left
+                {
+                    for (var i = 0; i < distance; i++)
+                    {
+                        x--;
+                        groundMap[y][x] = '#';
+                    }
+
+                    break;
+                }
+                case '3': // Up
                 {
                     for (var i = 0; i < distance; i++)
                     {
                         y--;
-                        groundMap[y, x] = '#';
+                        groundMap[y][x] = '#';
                     }
 
                     break;
@@ -120,15 +172,15 @@ public class Day18
         }
 
         // Dig out interior...
-        for (var i = 0; i < groundMap.GetLength(0); i++)
+        for (var i = 0; i < groundMap.Count; i++)
         {
             var insideTrench = false;
             char lastTile = '.';
 
-            for (var j = 0; j < groundMap.GetLength(1); j++)
+            for (var j = 0; j < groundMap[i].Length; j++)
             {
-                var currentTile = groundMap[i, j];
-                var nextTile = j == groundMap.GetLength(1) - 1 ? '#' : groundMap[i, j + 1];
+                var currentTile = groundMap[i][j];
+                var nextTile = j == groundMap[i].Length - 1 ? '#' : groundMap[i][j + 1];
 
                 switch (currentTile)
                 {
@@ -143,7 +195,7 @@ public class Day18
                     case '#' when lastTile == '#' && nextTile == '.':
                         // Look above us to see if we at at the ┘ corner of the trench boundry and therefore the next tile 
                         // is actually within the trench boundy.
-                        if (i > 0 && groundMap[i - 1, j + 1] == 'O')
+                        if (i > 0 && groundMap[i - 1][j + 1] == 'O')
                         {
                             insideTrench = true;
                         }
@@ -153,7 +205,7 @@ public class Day18
 
                     case '.' when insideTrench:
                         // Dig...
-                        groundMap[i, j] = 'O';
+                        groundMap[i][j] = 'O';
                         break;
                 }
 
@@ -161,16 +213,14 @@ public class Day18
             }
         }
 
-        // groundMap.WriteToFile("C:/temp/Day18_complete.txt"); // Debug
-
         // Count the size of the interior...
         int result = 0;
 
-        for (var i = 0; i < groundMap.GetLength(0); i++)
+        for (var i = 0; i < groundMap.Count; i++)
         {
-            for (var j = 0; j < groundMap.GetLength(1); j++)
+            for (var j = 0; j < groundMap[i].Length; j++)
             {
-                if (groundMap[i, j] == '#' || groundMap[i, j] == 'O') result++;
+                if (groundMap[i][j] == '#' || groundMap[i][j] == 'O') result++;
             }
         }
 
