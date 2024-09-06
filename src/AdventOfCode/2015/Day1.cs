69139515c053e34604e0215a0752b1c2b43dd9a6
Aaron <aaron@amorgan.net.nz>
2024-09-07 08:32:07 +1200

    2015, day 2, parts 1 & 2.

--- src/AdventOfCode/2015/Day1.cs
+++ src/AdventOfCode/2015/Day1.cs
@@ -8,18 +8,16 @@ public class Day1
     [Theory]
     [InlineData("Day1.txt", 280, Part.One)]
     [InlineData("Day1.txt", 1797, Part.Two)]
-    public void Day1_Part1_NotQuiteLisp(string filename, int expectedAnswer, Part problemPart)
+    public void Day1_Part1_Part2_NotQuiteLisp(string filename, int expectedAnswer, Part problemPart)
     {
-        string[] input = FileLoader.ReadAllLines("2015/" + filename).ToArray();
+        var input = FileLoader.ReadAllLines("2015/" + filename).ToArray();
         int floor = 0, index;
 
         for (index = 0; index < input[0].Length; index++)
         {
             if (problemPart == Part.Two && floor is -1) break;
 
-            var a = input[0][index];
-
-            switch (a)
+            switch (input[0][index])
             {
                 case '(':
                     floor++;
