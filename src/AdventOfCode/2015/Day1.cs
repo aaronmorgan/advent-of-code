e03fd252396e9b92ddc54c94497b7f830a7e379e
Aaron <aaron@amorgan.net.nz>
2024-09-07 00:21:15 +1200

    2015, day 1, part 1 & 2.

---
+++ src/AdventOfCode/2015/Day1.cs
@@ -0,0 +1,43 @@
+﻿using AdventOfCode.Types;
+using AdventOfCode.Utilities;
+
+namespace AdventOfCode._2015;
+
+public class Day1
+{
+    [Theory]
+    [InlineData("Day1.txt", 280, Part.One)]
+    [InlineData("Day1.txt", 1797, Part.Two)]
+    public void Day1_Part1_NotQuiteLisp(string filename, int expectedAnswer, Part problemPart)
+    {
+        string[] input = FileLoader.ReadAllLines("2015/" + filename).ToArray();
+        int floor = 0, index;
+
+        for (index = 0; index < input[0].Length; index++)
+        {
+            if (problemPart == Part.Two && floor is -1) break;
+
+            var a = input[0][index];
+
+            switch (a)
+            {
+                case '(':
+                    floor++;
+                    break;
+                default:
+                    floor--;
+                    break;
+            }
+        }
+
+        switch (problemPart)
+        {
+            case Part.One:
+                Assert.Equal(expectedAnswer, floor);
+                break;
+            case Part.Two:
+                Assert.Equal(expectedAnswer, index);
+                break;
+        }
+    }
+}
